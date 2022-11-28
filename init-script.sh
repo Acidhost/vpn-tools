8c33188bf77f708ae58b5212d0bd0951b5f96fd5
Acidhost <acidhost@outlook.com>
2022-11-28 13:48:04 +0300

    Slight changes

--- init-script.sh
+++ init-script.sh
@@ -10,8 +10,8 @@ mknod /dev/net/tun c 10 200
 # Iptables
 iptables -t nat -A POSTROUTING -o eth0 -s 192.168.103.0/24 -j MASQUERADE
 
-# Start Dante socks proxy
-sockd -D -f /vpn-tools/sockd/sockd.conf
-
 # Start OpenVPN service
-openvpn --config /vpn-tools/openvpn/server.conf
+openvpn --daemon --config /vpn-tools/openvpn/server.conf
+
+# Start Dante socks proxy
+sockd -f /vpn-tools/sockd/sockd.conf
--- openvpn-start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run -d -p 5190:1194/udp --cap-add=NET_ADMIN --mount type=bind,source=/etc/openvpn,target=/etc/openvpn ovpn
